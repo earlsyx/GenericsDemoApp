4ec2f9431050a759f2ae07c13475e55a95fcba68
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-25 07:31:44 +0800

    genreric in ehod generic in class infer

---
+++ HWGenerics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TakeItem(true);
+            Console.ReadLine();
+        }
+
+        public static void TakeItem<T>(T item)
+        {
+            T doubleItem = item;
+            Console.WriteLine($"Item: {doubleItem}");
+        }
+
+    }
+
+   
+
+}
